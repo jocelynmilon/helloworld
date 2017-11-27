bfd8bf2c4814aa5233204c3bb649b123b821a8cd
jocelyn <jocelyn.milon@gmail.com>
2017-11-27 15:35:06 +0100

    fin de la 4eme journéees

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
     selector: 'hello-world-app',
     templateUrl: 'app/app.component.html',
     styleUrls : ['app/app.component.css']
 })
-export class AppComponent { 
+export class AppComponent {
 
   titre = "projet Angular 2";
   date: Date = new Date();
@@ -18,12 +19,13 @@ export class AppComponent {
   images: string[] = ['app/images/image10.jpeg','app/images/image11.jpg','app/images/image12.jpg','app/images/image13.jpg','app/images/image14.jpg','app/images/image15.jpg'];
   picture: string = this.images[0];
   index: number = 0;
+  taches : string[] = ['Maj des infrastructures','Attirer les meilleurs jeune de la région', 'Construir une équipe compétitif'];
 
   onClick(){
     alert("vous avez cliquez sur le bouton");
   };
   changerImage(){
-   
+
    if (this.image == "app/image.jpeg"){                     // pour simplifier on peu ecrire  this.image =(this.image == "app/image.jpeg" ? "app/image2.jpg" : "app/image.jpeg")
      this.image = "app/image2.jpg"
    } else {
@@ -35,7 +37,7 @@ export class AppComponent {
       this.width = "1000"
       this.height = "600"
     }else {
-      this.width = "800" 
+      this.width = "800"
       this.height = "400"
     };
    }
@@ -52,16 +54,20 @@ export class AppComponent {
      this.nombre --
    }
    defilerImage(){
-    
+
     if(this.index == this.images.length -1){
       this.index = 0;
       this.picture = this.images[this.index]
-   
+
     }else{
       this.index ++;
       this.picture = this.images[this.index]
-    } 
+    }
 
 
    };
+   ajouterAListe(nouvelleTache:string) {
+     this.taches.push(nouvelleTache)
+     this.texte = '';
+   }
 }
